e29edbe1c4318e8a10d2fdbe53dd9a859336eafc
Phoomparin Mano <phoomparin@gmail.com>
2023-09-23 18:52:09 +0700

    feat: super duper dumb allocator

--- IDEAS.md
+++ IDEAS.md
@@ -1,4 +1,4 @@
-# 01: Memory
+## 01: Memory
 
 00 01 02 03 04 05 06 07
 AA BB CC DD EE FF 88 77
@@ -14,30 +14,17 @@ memset(offset: u16, byte: u8) -> u8 =
 memget(offset) = memory[offset]
 ```
 
-# 02: 
+## 02: Memory Allocation
 
 global alloc_cursor
 
-array(size) =
+alloc(size) =
   alloc_cursor += 1
   p = alloc_cursor
   alloc_cursor += size
 
   return ptr(p)
 
-array_set(ptr, index, value) =
-  memset(ptr + index, value)
-
-get_array(ptr, size) = 
-  index in 0..size:
-    memget(ptr + index)
-
-ptr a = array(5)
-array_set(a, 0, 0x11)
-array_set(a, 1, 0xFF)
-get_array(a, 5)
-
-ptr b = array(10)
-
-
-
+base = alloc(5)
+memset(base + 2, value)
+memget(base + 2)
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,47 @@
-use std::{process::Output, slice::SliceIndex};
-
 type M = [u8; 0xFF];
 
 #[derive(Debug)]
 struct CPU {
-    memory: M,
+    mem: M,
+    alloc_cursor: usize,
 }
 
 impl CPU {
     fn new() -> CPU {
-        CPU { memory: [0; 0xFF] }
+        CPU {
+            mem: [0; 0xFF],
+            alloc_cursor: 0,
+        }
     }
 
-    fn mset(&mut self, addr: usize, val: u8) {
-        self.memory[addr] = val;
+    /// Returns an offset to the allocated memory.
+    fn alloc(&mut self, size: usize) -> usize {
+        // Reserve the memory.
+        let p = self.alloc_cursor.clone();
+        self.alloc_cursor += size as usize;
+
+        return p;
+    }
+}
+
+trait Memory {
+    fn set(&mut self, addr: usize, val: u8);
+    fn get(&self, addr: usize) -> u8;
+}
+
+impl Memory for M {
+    fn set(&mut self, addr: usize, val: u8) {
+        self[addr] = val;
     }
 
-    fn mget(&self, addr: usize) -> u8 {
-        self.memory[addr]
+    fn get(&self, addr: usize) -> u8 {
+        self[addr]
     }
 }
 
 fn main() {
-    let mut cpu = CPU::new();
-    cpu.mset(0, 1);
-    println!("{}", cpu.mget(0));
+    let cpu = CPU::new();
+    println!("{}", cpu.mem.get(0));
 }
 
 #[cfg(test)]
@@ -32,11 +49,33 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_memory() {
+    fn test_memset() {
         let mut cpu = CPU::new();
 
-        cpu.mset(0, 1);
-        assert_eq!(cpu.mget(0), 1);
-        assert_eq!(cpu.mget(1), 0);
+        cpu.mem.set(0, 1);
+        assert_eq!(cpu.mem.get(0), 1);
+        assert_eq!(cpu.mem.get(1), 0);
+    }
+
+    #[test]
+    fn test_malloc() {
+        let mut cpu = CPU::new();
+        let mut m = cpu.mem;
+
+        let a = cpu.alloc(5);
+        m.set(a, 1);
+        m.set(a + 1, 2);
+        m.set(a + 4, 3);
+
+        let b = cpu.alloc(5);
+        m.set(b, 4);
+        m.set(b + 1, 5);
+
+        assert_eq!(a, 0);
+        assert_eq!(b, 5);
+
+        assert_eq!(&m[0..8], [1, 2, 0, 0, 3, 4, 5, 0]);
+
+        println!("{:?}", &m[0..8]);
     }
 }
