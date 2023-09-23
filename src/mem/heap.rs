41ce7f101ed0cc0d2ee67e4cb88fc28c91c1d175
Phoomparin Mano <phoomparin@gmail.com>
2023-09-24 02:30:02 +0700

    chore: remove HeapManager for now

--- src/mem/heap.rs
+++ src/mem/heap.rs
@@ -1,10 +1,10 @@
-use crate::mem::Memory;
-use crate::register::Register;
+// use crate::mem::Memory;
+// use crate::register::Register;
 
-pub struct HeapManager {
-    mem: Memory,
-    reg: Register
-}
+// pub struct HeapManager {
+//     mem: Memory,
+//     reg: Register
+// }
 
 // impl HeapManager {
 //     fn alloc(&mut self, _size: usize) -> u8 {
