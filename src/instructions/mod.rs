37d42fa0937373744d67fdc94471918460320756
Phoomparin Mano <phoomparin@gmail.com>
2023-09-24 12:45:07 +0700

    feat: convert instruction to opcode

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -5,9 +5,9 @@ pub use bimap::BiMap;
 use lazy_static::lazy_static;
 
 use strum::IntoEnumIterator;
-use strum_macros::EnumIter;
+use strum_macros::{EnumIter, FromRepr};
 
-#[derive(Debug, Copy, Clone, Eq, PartialEq, EnumIter, Hash)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, EnumIter, Hash, FromRepr)]
 pub enum Instruction {
     None,
 
@@ -36,18 +36,26 @@ lazy_static! {
 
 type I = Instruction;
 
-impl Instruction {
-    pub fn id(id: u8) -> Instruction {
-        *OPCODES.get_by_right(&id).unwrap_or(&Instruction::None)
+impl From<u8> for Instruction {
+    fn from(id: u8) -> Self {
+        Instruction::from_repr(id as usize).unwrap_or(I::None)
     }
+}
 
-    pub fn opcode(self) -> u8 {
-        let v = match self {
+impl From<Instruction> for u8 {
+    fn from(ins: Instruction) -> Self {
+        let v = match ins {
             I::Push(_) => I::Push(0),
-            _ => self
+            _ => ins
         };
 
         *OPCODES.get_by_left(&v).unwrap_or(&0)
+    }
+}
+
+impl Instruction {
+    pub fn opcode(self) -> u8 {
+        self.into()
     }
 }
 
@@ -56,10 +64,16 @@ mod tests {
     use super::*;
     #[test]
     fn test_opcode() {
-        let v = I::Push(15);
-        println!("{:?} {:?}", v, v.opcode());
+        // Convert instruction to opcode.
+        assert_eq!(I::Push(12).opcode(), 0x01);
 
-        // assert_eq!(I::Push(0).opcode(), 0x01);
-        // assert_eq!(I::Sub.opcode(), 0x04);
+        // Convert opcode to instruction.
+        assert_eq!(I::from(2), I::Pop);
+
+        // Convert opcode to instruction.
+        assert_eq!(I::from(I::Pop.opcode()), I::Pop);
+
+        // Convert instruction to opcode and back.
+        assert_eq!(I::from(I::Push(12).opcode()), I::Push(0));
     }
 }
