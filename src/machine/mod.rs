caeae806f0a44a5b4e840c22b7b12d4e06667913
Phoomparin Mano <phoomparin@gmail.com>
2023-09-24 15:11:45 +0700

    refactor: include StackManager as a method in machine

--- src/machine/mod.rs
+++ src/machine/mod.rs
@@ -2,17 +2,12 @@ use snafu::Whatever;
 use crate::mem::{Memory, StackManager};
 use crate::register::Registers;
 use crate::register::Register::PC;
-use crate::instructions::{Instruction, Load};
 use crate::instructions::Instruction as I;
 
 #[derive(Debug)]
 pub struct Machine {
     pub mem: Memory,
     pub reg: Registers,
-}
-
-fn stack(m: &mut Machine) -> StackManager {
-    StackManager::new(&mut m.mem, &mut m.reg)
 }
 
 impl Machine {
@@ -27,24 +22,25 @@ impl Machine {
         }
     }
 
+    fn stack(&mut self) -> StackManager {
+        StackManager::new(&mut self.mem, &mut self.reg)
+    }
+
     pub fn tick(&mut self) -> Result<(), Whatever> {
         let pc = self.reg.get(PC);
         let op_code = self.mem.get(pc);
         let op: I = op_code.into();
 
-
-
         match op {
             I::None => {}
             I::Push(_) => {
                 let arg = self.mem.get(pc + 1);
-                let mut s = stack(self);
-                s.push(arg).unwrap();
+                self.stack().push(arg).unwrap();
             },
             I::Pop => {
-                let mut s = stack(self);
-                let v = s.pop();
-                println!("Pop: {:?}", v);
+                // TODO: what to do with the popped value?
+                let v = self.stack().pop().unwrap();
+                println!("Pop: {}", v);
             }
             I::Add => {
                 // TODO: implement ADD
