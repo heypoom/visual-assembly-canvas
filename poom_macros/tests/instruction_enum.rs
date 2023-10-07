e4866fbdcc5217c9ad128f4b86718f21cabaa389
Phoomparin Mano <phoomparin@gmail.com>
2023-10-08 01:35:30 +0700

    test: combine with_arg and arity proc macro tests

--- poom_macros/tests/instruction_enum.rs
+++ poom_macros/tests/instruction_enum.rs
@@ -1,8 +1,8 @@
 #[cfg(test)]
 mod instruction_enum_tests {
-    use poom_macros::InsertArgs;
+    use poom_macros::{InsertArgs, Arity};
 
-    #[derive(Debug, PartialEq, InsertArgs)]
+    #[derive(Debug, PartialEq, InsertArgs, Arity)]
     enum Foo {
         Pop,
         Push(u16),
@@ -14,5 +14,12 @@ mod instruction_enum_tests {
         assert_eq!(Foo::Pop.with_arg(|| 12), Foo::Pop);
         assert_eq!(Foo::Push(0).with_arg(|| 24), Foo::Push(24));
         assert_eq!(Foo::Foo(0, 0, 0).with_arg(|| 6), Foo::Foo(6, 6, 6));
+    }
+
+    #[test]
+    fn test_arity() {
+        assert_eq!(Foo::Pop.arity(), 0);
+        assert_eq!(Foo::Push(1).arity(), 1);
+        assert_eq!(Foo::Foo(0, 0, 0).arity(), 3);
     }
 }
