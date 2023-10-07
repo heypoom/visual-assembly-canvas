d189cfc258d0ab610fed7e8878fad5321ee976e2
Phoomparin Mano <phoomparin@gmail.com>
2023-10-07 16:18:07 +0700

    chore: reformatting the call stack tests

--- tests/call_stack.rs
+++ tests/call_stack.rs
@@ -5,8 +5,14 @@ mod call_stack_tests {
     extern crate opcodes_to_algorithms as O;
 
     use super::code_samples::CODE_SAMPLE_CALL_STACK;
-
     use O::{Machine, Execute, Instruction as I};
+
+    #[test]
+    fn test_call_stack_asm() {
+        let mut m: Machine = CODE_SAMPLE_CALL_STACK.into();
+        m.run();
+        assert_eq!(m.mem.read_stack(6), [0xAA, 0b11001100, 1024, 0xAA, 0b11001100, 1024]);
+    }
 
     #[test]
     fn test_call_stack_instructions() {
@@ -21,12 +27,5 @@ mod call_stack_tests {
 
         m.run();
         assert_eq!(m.mem.read_stack(4), [0xAA, 0xBB, 0xAA, 0xBB]);
-    }
-
-    #[test]
-    fn test_call_stack_asm() {
-        let mut m: Machine = CODE_SAMPLE_CALL_STACK.into();
-        m.run();
-        assert_eq!(m.mem.read_stack(6), [0xAA, 0b11001100, 1024, 0xAA, 0b11001100, 1024]);
     }
 }
