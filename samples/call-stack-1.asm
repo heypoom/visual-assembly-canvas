02880c568a90a3be9f2012d1f7794eb4345e15c0
Phoomparin Mano <phoomparin@gmail.com>
2023-10-07 16:36:04 +0700

    feat: add an executable program for interpreter

---
+++ samples/call-stack-1.asm
@@ -0,0 +1,11 @@
+jump start
+
+add_pattern:
+    push 0xAA        ; 170
+    push 0b11001100  ; 204
+    push 01024       ; 1024
+    return
+
+start:
+    call add_pattern
+    call add_pattern
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
-use opcodes_to_algorithms::{Execute, Instruction as I, Load, Machine, WithStringManager};
+use std::{env, io};
+use std::fs::File;
+use std::io::Read;
+use opcodes_to_algorithms::{Execute, Machine};
 
-fn main() {
-    let mut m = Machine::new();
-    m.handlers.print.push(Box::new(|s| print!("{}", s)));
+fn read_input() -> Result<String, io::Error> {
+    let args: Vec<String> = env::args().collect();
 
-    let welcome_ptr = m.mem.string().add_str("hello, world!");
-    m.mem.load_code(vec![I::Push(welcome_ptr), I::Print]);
-    m.run();
+    if args.len() > 1 {
+        let path = args.get(1).unwrap();
+        let mut file = File::open(path)?;
+        let mut content = String::new();
+        file.read_to_string(&mut content)?;
+
+        return Ok(content);
+    }
+
+    let mut content = String::new();
+    io::stdin().read_to_string(&mut content)?;
+
+    Ok(content)
 }
+
+fn main() -> Result<(), io::Error> {
+    let source = read_input()?;
+
+    let mut m: Machine = (&*source).into();
+    m.run();
+
+    println!("{:?}", m.mem.read_stack(10));
+    Ok(())
+}
+
