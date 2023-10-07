51742094c71cd7d91cb31181493bab81d4c2e9c0
Phoomparin Mano <phoomparin@gmail.com>
2023-10-07 23:48:54 +0700

    chore: rename ins to op

--- src/instructions/compile.rs
+++ src/instructions/compile.rs
@@ -3,12 +3,12 @@ use crate::{Instruction as I};
 pub fn compile(ops: Vec<I>) -> Vec<u16> {
     let mut bytecode = vec![];
 
-    for ins in ops {
-        bytecode.push(ins.opcode());
+    for op in ops {
+        bytecode.push(op.opcode());
 
         // Insert the arguments into memory.
         // TODO: this is very repetitive!
-        if let I::LoadString(v) | I::Store(v) | I::Load(v) | I::JumpNotZero(v) | I::JumpZero(v) | I::Jump(v) | I::Push(v) | I::Call(v) = ins {
+        if let I::LoadString(v) | I::Store(v) | I::Load(v) | I::JumpNotZero(v) | I::JumpZero(v) | I::Jump(v) | I::Push(v) | I::Call(v) = op {
             bytecode.push(v)
         }
     }
