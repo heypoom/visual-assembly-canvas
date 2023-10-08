e01670374fd49efeb1fa31ebe6d3e516231f8d9b
Phoomparin Mano <phoomparin@gmail.com>
2023-10-08 18:48:41 +0700

    feat: setup webassembly skeleton

--- visual-assembly-canvas/vite.config.ts
+++ visual-assembly-canvas/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite'
 
-// https://vitejs.dev/config/
+import react from '@vitejs/plugin-react'
+import awaits from 'vite-plugin-top-level-await'
+import {ViteRsw} from 'vite-plugin-rsw'
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), awaits(), ViteRsw()],
 })
