85d58aaaed3156f355c11ca1717e3f89454d64a1
Phoomparin Mano <phoomparin@gmail.com>
2023-10-31 20:36:03 +0700

    feat: add a function to highlight source line

--- canvas/src/inspector/utils/getHighlightedSourceLine.test.ts
+++ canvas/src/inspector/utils/getHighlightedSourceLine.test.ts
@@ -2,12 +2,33 @@ import { expect, test } from "vitest"
 import { getHighlightedSourceLine } from "./getHighlightedSourceLine"
 
 test("should be able to get highlighted source line", () => {
-  const source = `
+  const S1 = `
     push 5
     push 10
     push 15
     push 1024
   `
 
-  expect(getHighlightedSourceLine(source, 8)).toBe(3)
+  expect(getHighlightedSourceLine(S1, 8)).toBe(4)
+
+  const S2 = `
+    .string hello_world "Hello, world!"
+    .string foo "foo bar"
+
+    load_string hello_world
+    print
+
+    .string sunshine "Sunshine!"
+
+    load_string sunshine
+    print
+
+    .value bar 0xDEAD
+    .value baz 0xAAAA
+
+    push bar
+    push baz
+  `
+
+  expect(getHighlightedSourceLine(S2, 8)).toBe(15)
 })
--- canvas/src/inspector/utils/getHighlightedSourceLine.ts
+++ canvas/src/inspector/utils/getHighlightedSourceLine.ts
@@ -1,6 +1,27 @@
-export function getHighlightedSourceLine(source: string, pc: number) {
-  source = source.trim()
-
+export function getHighlightedSourceLine(
+  source: string,
+  pc: number,
+): number | null {
   const lines = source.split("\n")
-  lines //?
+
+  let curr = 0
+  let currLine = 0
+
+  for (const line of lines) {
+    const [opcode, ...args] = line.trim().split(" ")
+
+    currLine++
+
+    // Skip comments and directives.
+    if (line.length === 0) continue
+    if (opcode.startsWith("//")) continue
+    if (opcode.startsWith(".")) continue
+
+    const argCount = args.length
+    curr += 1 + argCount
+
+    if (curr >= pc) return currLine - 1
+  }
+
+  return null
 }
