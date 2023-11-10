7d833879b93734db5ffec2ba03bd037b5b9b10b8
Phoomparin Mano <phoomparin@gmail.com>
2023-11-11 04:21:25 +0700

    feat: group hover for machine

--- canvas/src/canvas/blocks/machine/MachineBlock.tsx
+++ canvas/src/canvas/blocks/machine/MachineBlock.tsx
@@ -9,15 +9,15 @@ export function MachineBlockView(props: NodeProps<MachineBlock>) {
   const { data } = props
 
   return (
-    <div className="font-mono bg-slate-1 relative">
+    <div className="font-mono bg-slate-1 relative group">
       <Handle
         type="target"
         position={Position.Left}
         id="0"
-        className="bg-crimson-9 px-1 py-1 ml-[-1px] border-2"
+        className="bg-crimson-9 group-hover:bg-cyan-11 hover:!bg-gray-12 hover:border-crimson-9 px-1 py-1 ml-[-1px] border-2 z-10"
       ></Handle>
 
-      <div className="px-3 py-3 border-2 rounded-2">
+      <div className="px-3 py-3 border-2 rounded-2 hover:border-cyan-11">
         <div className="flex flex-col space-y-2 text-gray-50">
           <div className="min-h-[100px]">
             <div className="nodrag">
@@ -33,7 +33,7 @@ export function MachineBlockView(props: NodeProps<MachineBlock>) {
         type="source"
         position={Position.Right}
         id="1"
-        className="bg-crimson-9 px-1 py-1 mr-[-1px] border-2"
+        className="bg-crimson-9 group-hover:bg-cyan-11 hover:!bg-gray-12 hover:border-crimson-9 px-1 py-1 mr-[-1px] border-2 z-10"
       />
     </div>
   )
