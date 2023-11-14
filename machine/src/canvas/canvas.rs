cbb1bd79d6e0931b182922ed2d1c4b4c72541d05
Phoomparin Mano <phoomparin@gmail.com>
2023-11-15 00:36:47 +0700

    feat: clock must update time then send the data

--- machine/src/canvas/canvas.rs
+++ machine/src/canvas/canvas.rs
@@ -217,23 +217,8 @@ impl Canvas {
     }
 
     pub fn tick_clock_block(&mut self, id: u16, messages: Vec<Message>) -> Errorable {
-        let block = self.get_block(id)?;
-        let Clock { time } = block.data else { return Ok(()); };
-
-        self.send_data_to_sinks(id, vec![time])?;
-
         let block = self.mut_block(id)?;
         let Clock { time } = &mut block.data else { return Ok(()); };
-
-        for message in &messages {
-            match &message.action {
-                Action::Reset => {
-                    *time = 0;
-                }
-
-                _ => {}
-            }
-        }
 
         // increment the time, or wrap around to 0.
         *time = (*time).checked_add(1).unwrap_or(0);
@@ -242,6 +227,20 @@ impl Canvas {
         if *time >= 255 {
             *time = 0;
         }
+
+        for message in &messages {
+            match &message.action {
+                Action::Reset => {
+                    *time = 0;
+                }
+                _ => {}
+            }
+        }
+
+        // Send data to sinks
+        let block = self.get_block(id)?;
+        let Clock { time } = block.data else { return Ok(()); };
+        self.send_data_to_sinks(id, vec![time])?;
 
         Ok(())
     }
