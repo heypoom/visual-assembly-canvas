c369a0e10db8c82b3e47df4362707ab72b830215
Phoomparin Mano <phoomparin@gmail.com>
2023-11-15 19:09:22 +0700

    chore: log midi errors

--- canvas/src/midi/event.ts
+++ canvas/src/midi/event.ts
@@ -3,47 +3,51 @@ import { $lastMidiEvent } from "../store/midi"
 import { MidiEffect } from "../types/effects"
 
 export async function processMidiEvent(id: number, effect: MidiEffect) {
-  if (!effect) return
+  try {
+    if (!effect) return
 
-  const midi = effect.Midi
-  if (!midi) return
-  if (!midiManager.initialized) await midiManager.setup()
+    const midi = effect.Midi
+    if (!midi) return
+    if (!midiManager.initialized) await midiManager.setup()
 
-  $lastMidiEvent.setKey(id, effect)
+    $lastMidiEvent.setKey(id, effect)
 
-  const { format, data, port, channel } = midi
-  const output = midiManager.outputs[port]
-  if (!output) return
+    const { format, data, port, channel } = midi
+    const output = midiManager.outputs[port]
+    if (!output) return
 
-  switch (format) {
-    case "Raw": {
-      output?.send(data)
-      return
+    switch (format) {
+      case "Raw": {
+        output?.send(data)
+        return
+      }
+
+      case "Note": {
+        if (data.length < 2) return
+
+        const [note, rawAttack] = data
+        output?.playNote(note, { rawAttack, channels: channel })
+
+        return
+      }
+
+      case "ControlChange": {
+        if (data.length < 2) return
+
+        const [controller, value] = data
+        output?.sendControlChange(controller, value, { channels: channel })
+
+        return
+      }
+
+      case "Launchpad": {
+        if (!launchpad.initialized) launchpad.setup(midiManager)
+
+        launchpad.cmd(...data)
+        return
+      }
     }
-
-    case "Note": {
-      if (data.length < 2) return
-
-      const [note, rawAttack] = data
-      output?.playNote(note, { rawAttack, channels: channel })
-
-      return
-    }
-
-    case "ControlChange": {
-      if (data.length < 2) return
-
-      const [controller, value] = data
-      output?.sendControlChange(controller, value, { channels: channel })
-
-      return
-    }
-
-    case "Launchpad": {
-      if (!launchpad.initialized) launchpad.setup(midiManager)
-
-      launchpad.cmd(...data)
-      return
-    }
+  } catch (error) {
+    console.warn("midi error:", error)
   }
 }
