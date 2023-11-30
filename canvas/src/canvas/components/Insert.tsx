9199e260cbbcedbfbf96bbf11773dd6d6bc42394
Phoomparin Mano <phoomparin@gmail.com>
2023-11-30 23:53:19 +0700

    feat: slash command selector

--- canvas/src/canvas/components/Insert.tsx
+++ canvas/src/canvas/components/Insert.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useMemo, useState } from "react"
 import { getMatchedCommands, useCommandRunner } from "../commands/commands"
 import { useHotkeys } from "react-hotkeys-hook"
+import cn from "classnames"
 
 type Pos = { x: number; y: number } | null
 
@@ -8,20 +9,17 @@ export function Insert() {
   const [command, setCommand] = useState("")
   const [cursor, setCursor] = useState<Pos>(null)
   const [active, setActive] = useState(false)
+  const [selected, setSelected] = useState(0)
 
   const { run } = useCommandRunner()
 
   const matches = useMemo(() => getMatchedCommands(command), [command])
 
   useHotkeys("/", () => {
-    setActive(!active)
+    setActive((active) => !active)
   })
 
-  useHotkeys("esc", () => {
-    disable()
-  })
-
-  function disable() {
+  function hide() {
     setActive(false)
     setCommand("")
   }
@@ -55,34 +53,59 @@ export function Insert() {
   return (
     <div>
       <div
-        className="fixed font-mono px-4 py-3 bg-gray-5 rounded-3"
+        className="flex flex-col fixed font-mono px-4 py-3 bg-gray-5 rounded-3 gap-y-2"
         style={{ top: `${top}px`, left: `${left}px` }}
       >
         <input
           className="bg-transparent text-4 outline-none"
           value={command}
           autoFocus
-          onChange={(e) => setCommand(e.target.value)}
+          onChange={(e) => {
+            setSelected(0)
+            setCommand(e.target.value)
+          }}
           onKeyDown={(e) => {
+            if (e.key === "ArrowDown") {
+              e.preventDefault()
+              setSelected((s) => Math.min(s + 1, matches.length - 1))
+              return
+            }
+
+            if (e.key === "ArrowUp") {
+              e.preventDefault()
+              setSelected((s) => Math.max(s - 1, 0))
+              return
+            }
+
             if (e.key === "Enter") {
               if (matches.length === 0) return
 
-              const [match] = matches
+              const ok = run(matches[selected], { position: cursor })
+              if (ok) hide()
 
-              const ok = run(match, { position: cursor })
-              if (ok) disable()
+              return
+            }
+
+            if (e.key === "Escape") {
+              hide()
+              return
             }
           }}
         />
 
-        <div>
-          {matches.map((preview) => (
-            <div key={preview.prefix} className="flex gap-x-2">
-              <div className="text-2">/{preview.prefix}</div>
-              <div className="text-2">{preview.name}</div>
-            </div>
-          ))}
-        </div>
+        {matches.length > 0 && (
+          <div>
+            {matches.map((preview, i) => (
+              <div
+                key={preview.prefix}
+                className={cn("flex gap-x-2", selected === i && "text-red-11")}
+              >
+                <div className="text-2">/{preview.prefix}</div>
+                <div className="text-2">{preview.name}</div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   )
