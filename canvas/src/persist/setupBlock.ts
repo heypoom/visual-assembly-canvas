dc3efc32168d97c3765b76539907807e1e13f567
Phoomparin Mano <phoomparin@gmail.com>
2023-11-30 16:55:22 +0700

    feat: load machine on load

--- canvas/src/persist/setupBlock.ts
+++ canvas/src/persist/setupBlock.ts
@@ -1,9 +1,14 @@
 import { BlockNode } from "../types/Node"
 import { isBlock } from "../blocks"
 import { audioManager } from "../services/audio/manager"
+import { engine } from "../engine"
 
 export function setupBlock(block: BlockNode) {
   const { id } = block.data
+
+  if (isBlock.machine(block)) {
+    engine.load(id, block.data.source)
+  }
 
   if (isBlock.synth(block)) {
     audioManager.add(id, block.data.config)
