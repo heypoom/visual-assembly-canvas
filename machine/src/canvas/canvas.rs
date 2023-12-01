8761a3df20d4db09feb1c69094e7f0eecb78da9e
Phoomparin Mano <phoomparin@gmail.com>
2023-12-02 04:25:50 +0700

    feat: use duration as address

--- machine/src/canvas/canvas.rs
+++ machine/src/canvas/canvas.rs
@@ -292,13 +292,14 @@ impl Canvas {
                     let block = self.mut_block(id)?;
 
                     let Synth { synth_id, .. } = block.data else { continue; };
-                    let [time] = data[..] else { continue; };
+                    let [note] = data[..] else { continue; };
+                    let duration = *address + 1;
 
                     block.events.push(Event::Synth {
                         target: synth_id,
                         triggers: vec![AttackRelease {
-                            freq: note_to_freq(*address as u8),
-                            duration: (time as f32) / 255f32,
+                            freq: note_to_freq(note as u8),
+                            duration: (duration as f32) / 255f32,
                             time: 0.0,
                         }],
                     })
