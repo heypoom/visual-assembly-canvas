2b2e40920f49cbef5cd31d7541b9856b3570aa0a
Phoomparin Mano <phoomparin@gmail.com>
2023-12-01 20:44:59 +0700

    chore: swap handle id

--- canvas/src/blocks/machine/index.tsx
+++ canvas/src/blocks/machine/index.tsx
@@ -26,7 +26,7 @@ export function MachineBlock(props: NodeProps<MachineProps>) {
       <Handle
         type="target"
         position={Position.Left}
-        id="0"
+        id="1"
         className="bg-crimson-9 group-hover:bg-cyan-11 hover:!bg-gray-12 hover:border-crimson-9 px-1 py-1 ml-[-1px] border-2 z-10"
       ></Handle>
 
@@ -54,7 +54,7 @@ export function MachineBlock(props: NodeProps<MachineProps>) {
       <Handle
         type="source"
         position={Position.Right}
-        id="1"
+        id="0"
         className="bg-crimson-9 group-hover:bg-cyan-11 hover:!bg-gray-12 hover:border-crimson-9 px-1 py-1 mr-[-1px] border-2 z-10"
       />
     </div>
