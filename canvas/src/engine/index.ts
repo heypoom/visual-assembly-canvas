d9b8b9b41e1296ced4f1a3b9a4623be886dc3086
Phoomparin Mano <phoomparin@gmail.com>
2023-12-01 13:46:47 +0700

    chore: remove logs

--- canvas/src/engine/index.ts
+++ canvas/src/engine/index.ts
@@ -86,8 +86,6 @@ export class CanvasEngine {
   }
 
   public load(id: number, source: string, force?: boolean) {
-    console.log(`engine::load`, id, source, force)
-
     // If the source is the same, we don't need to reload.
     if (this.sources.get(id) === source && !force) return
     this.sources.set(id, source)
