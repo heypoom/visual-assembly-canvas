6fb894db67af04d8fdeb7cf6250cdc3eb34ab812
Phoomparin Mano <phoomparin@gmail.com>
2023-12-02 17:56:14 +0700

    feat: bitpacking example

--- machine/tests/asm/bitpacking.asm
+++ machine/tests/asm/bitpacking.asm
@@ -1,25 +1,29 @@
-push 1
-push 0
-push 1
-push 0
-push 1
+.value LEN 4
+.value PACKED_PTR 0x5000
 
-push 0 ; accumulator
-push 5 ; pack 5 bits
+push 0
+store PACKED_PTR ; *packed_ptr = 0
+push 0 ; [i = 0]
 
-pack_loop:
-  dup
-  jump_zero pack_end
-  swap
-  pop
-  swap
+loop:
+  dup          ; [i, i]
+  push 0x2000  ; [i, i, 0x2000]
+  add          ; [i, 0x20XX]
+  read 1       ; [i, A]
+
+  load PACKED_PTR
   push 1
   left_shift
+  swap
   or
-  swap
-  dec
-  swap
-  jump pack_loop
+  store PACKED_PTR
 
-pack_end:
-  push 0xDEAD
+  swap         ; [A, i]
+  inc          ; [A, i + 1]
+  dup          ; [A, i, i]
+  push LEN     ; [A, i, i, MAX]
+
+  greater_than_or_equal ; MAX >= i
+  jump_zero loop ; [i]
+  pop ; discard [i]
+  load PACKED_PTR ; load the packed number
