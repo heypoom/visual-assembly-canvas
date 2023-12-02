d41a9eab060a9bf66ca2f59c26ef7eb6286b2d15
Phoomparin Mano <phoomparin@gmail.com>
2023-12-02 18:00:38 +0700

    chore: update bitpacking example

--- machine/tests/asm/bitpacking.asm
+++ machine/tests/asm/bitpacking.asm
@@ -6,24 +6,23 @@ store PACKED_PTR ; *packed_ptr = 0
 push 0 ; [i = 0]
 
 loop:
-  dup          ; [i, i]
-  push 0x2000  ; [i, i, 0x2000]
-  add          ; [i, 0x20XX]
-  read 1       ; [i, A]
+dup          ; [i, i]
+push 0x2000  ; [i, i, 0x2000]
+add          ; [i, 0x20XX]
+read 1       ; [i, B]
 
-  load PACKED_PTR
-  push 1
-  left_shift
-  swap
-  or
-  store PACKED_PTR
+load PACKED_PTR  ; [i, B, P]
+push 1           ; [i, B, P, 1]
+left_shift       ; [i, B, P << 1]
+swap             ; [i, P << 1, B]
+or               ; [i, P << 1 | B]
+store PACKED_PTR ; [i]
 
-  swap         ; [A, i]
-  inc          ; [A, i + 1]
-  dup          ; [A, i, i]
-  push LEN     ; [A, i, i, MAX]
+inc          ; [i++]
+dup          ; [i, i]
+push LEN     ; [i, i, MAX]
 
-  greater_than_or_equal ; MAX >= i
-  jump_zero loop ; [i]
-  pop ; discard [i]
-  load PACKED_PTR ; load the packed number
+greater_than_or_equal ; MAX >= i
+jump_zero loop ; [i]
+pop ; discard [i] 
+load PACKED_PTR ; load the packed number
