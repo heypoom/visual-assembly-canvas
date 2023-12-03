75f9cc1d95cb10699e741e28c5ba2ec85b59add3
Phoomparin Mano <phoomparin@gmail.com>
2023-12-03 18:51:50 +0700

    refactor: use base block for pixel block

--- canvas/src/blocks/pixel/index.tsx
+++ canvas/src/blocks/pixel/index.tsx
@@ -1,12 +1,9 @@
-import { EyeClosedIcon, MixerHorizontalIcon } from "@radix-ui/react-icons"
 import { TextField } from "@radix-ui/themes"
-import cn from "classnames"
 import { PixelMode as _PixelMode } from "machine-wasm"
-import { useEffect, useReducer, useRef } from "react"
+import { useEffect, useRef } from "react"
 import { NodeProps } from "reactflow"
 
-import { getPixelColor, PaletteKey, palettes } from "@/blocks"
-import { BlockHandle, RightClickMenu } from "@/blocks/components"
+import { BaseBlock, getPixelColor, PaletteKey, palettes } from "@/blocks"
 import { engine } from "@/engine"
 import { updateNodeData } from "@/store/blocks"
 import type { PixelProps } from "@/types/blocks"
@@ -32,7 +29,6 @@ export const PixelBlock = (props: NodeProps<PixelProps>) => {
   const { columns = 9, palette = "base", mode = "Append" } = data
 
   const canvasRef = useRef<HTMLCanvasElement | null>()
-  const [isSettings, toggle] = useReducer((n) => !n, false)
 
   const pixels =
     data.pixels?.length > 0 ? data.pixels : [...Array(columns * 5)].fill(0)
@@ -77,91 +73,69 @@ export const PixelBlock = (props: NodeProps<PixelProps>) => {
     }
   }, [pixels, columns, palette])
 
-  return (
-    <div className="group">
-      <BlockHandle port={1} side="left" type="target" />
+  const Settings = () => (
+    <div className="flex flex-col font-mono bg-gray-1">
+      <div className="flex flex-col max-w-[160px] gap-y-3 px-3 py-3">
+        <div className="flex items-center gap-4">
+          <p className="text-1">Columns</p>
 
-      <RightClickMenu id={id} show={isSettings} toggle={toggle}>
-        <div
-          className={cn(
-            "border-2 border-crimson-9 rounded-2 relative hover:border-cyan-9",
-            props.selected && "!border-yellow-11",
-          )}
-        >
-          <div
-            className="absolute hidden group-hover:flex right-2 top-1 bg-gray-1 p-1 rounded-6 text-cyan-11 hover:bg-cyan-9 hover:text-gray-1"
-            onClick={toggle}
-          >
-            {isSettings ? <EyeClosedIcon /> : <MixerHorizontalIcon />}
-          </div>
+          <TextField.Input
+            placeholder="8"
+            value={columns === 0 ? "" : columns.toString()}
+            size="1"
+            min={2}
+            max={16}
+            className="!w-[70px]"
+            onChange={(e) => {
+              const value = parseInt(e.target.value)
+              if (isNaN(value)) return update({ columns: 0 })
 
-          <div className="">
-            {!isDrawable && (
-              <div className="px-4 py-3 font-mono text-1 text-crimson-10">
-                Columns must be more than 1!
-              </div>
-            )}
-
-            {isDrawable && (
-              <div
-                className=""
-                style={{ height: `${height}px`, width: `${width}px` }}
-              >
-                <canvas
-                  ref={(r) => (canvasRef.current = r)}
-                  className="h-[180px]"
-                  style={{ height, width }}
-                />
-              </div>
-            )}
-          </div>
-
-          {isSettings && (
-            <div className="flex flex-col font-mono bg-gray-1">
-              <div className="flex flex-col max-w-[160px] gap-y-3 px-3 py-3">
-                <div className="flex items-center gap-4">
-                  <p className="text-1">Columns</p>
-
-                  <TextField.Input
-                    placeholder="8"
-                    value={columns === 0 ? "" : columns.toString()}
-                    size="1"
-                    min={2}
-                    max={16}
-                    className="!w-[70px]"
-                    onChange={(e) => {
-                      const value = parseInt(e.target.value)
-                      if (isNaN(value)) return update({ columns: 0 })
-
-                      update({ columns: value })
-                    }}
-                  />
-                </div>
-
-                <div className="flex items-center gap-4 w-full">
-                  <p className="text-1">Palette</p>
-
-                  <RadixSelect
-                    value={palette}
-                    onChange={(p) => update({ palette: p as PaletteKey })}
-                    options={paletteOptions}
-                  />
-                </div>
-
-                <div className="flex items-center gap-4 w-full">
-                  <p className="text-1">Behavior</p>
-
-                  <RadixSelect
-                    value={mode.toString()}
-                    onChange={(p) => update({ mode: p as PixelMode })}
-                    options={modeOptions}
-                  />
-                </div>
-              </div>
-            </div>
-          )}
+              update({ columns: value })
+            }}
+          />
         </div>
-      </RightClickMenu>
+
+        <div className="flex items-center gap-4 w-full">
+          <p className="text-1">Palette</p>
+
+          <RadixSelect
+            value={palette}
+            onChange={(p) => update({ palette: p as PaletteKey })}
+            options={paletteOptions}
+          />
+        </div>
+
+        <div className="flex items-center gap-4 w-full">
+          <p className="text-1">Behavior</p>
+
+          <RadixSelect
+            value={mode.toString()}
+            onChange={(p) => update({ mode: p as PixelMode })}
+            options={modeOptions}
+          />
+        </div>
+      </div>
     </div>
+  )
+
+  return (
+    <BaseBlock node={props} targets={1} settings={Settings}>
+      {isDrawable ? (
+        <div
+          className=""
+          style={{ height: `${height}px`, width: `${width}px` }}
+        >
+          <canvas
+            ref={(r) => (canvasRef.current = r)}
+            className="h-[180px]"
+            style={{ height, width }}
+          />
+        </div>
+      ) : (
+        <div className="px-4 py-3 font-mono text-1 text-tomato-11">
+          There must be at least two columns.
+        </div>
+      )}
+    </BaseBlock>
   )
 }
