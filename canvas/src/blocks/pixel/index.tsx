46fd09be2a4559bcc0b758017f9bb828aa0d916e
Phoomparin Mano <phoomparin@gmail.com>
2023-12-03 14:21:26 +0700

    feat: add context menu to pixels

--- canvas/src/blocks/pixel/index.tsx
+++ canvas/src/blocks/pixel/index.tsx
@@ -5,7 +5,7 @@ import { useEffect, useReducer, useRef } from "react"
 import { NodeProps } from "reactflow"
 
 import { getPixelColor, PaletteKey, palettes } from "@/blocks"
-import { BlockHandle } from "@/blocks/components"
+import { BlockHandle, RightClickMenu } from "@/blocks/components"
 import { engine } from "@/engine"
 import { updateNodeData } from "@/store/blocks"
 import type { PixelProps } from "@/types/blocks"
@@ -80,80 +80,82 @@ export const PixelBlock = (props: NodeProps<PixelProps>) => {
     <div className="group">
       <BlockHandle port={1} side="left" type="target" />
 
-      <div className="border-2 border-crimson-9 rounded-2 relative hover:border-cyan-9">
-        <div
-          className="absolute hidden group-hover:flex right-2 top-1 bg-gray-1 p-1 rounded-6 text-cyan-11 hover:bg-cyan-9 hover:text-gray-1"
-          onClick={toggle}
-        >
-          {isSettings ? <EyeClosedIcon /> : <MixerHorizontalIcon />}
-        </div>
-
-        <div className="">
-          {!isDrawable && (
-            <div className="px-4 py-3 font-mono text-1 text-crimson-10">
-              Columns must be more than 1!
-            </div>
-          )}
-
-          {isDrawable && (
-            <div
-              className=""
-              style={{ height: `${height}px`, width: `${width}px` }}
-            >
-              <canvas
-                ref={(r) => (canvasRef.current = r)}
-                className="h-[180px]"
-                style={{ height, width }}
-              />
-            </div>
-          )}
-        </div>
-
-        {isSettings && (
-          <div className="flex flex-col font-mono bg-gray-1">
-            <div className="flex flex-col max-w-[160px] gap-y-3 px-3 py-3">
-              <div className="flex items-center gap-4">
-                <p className="text-1">Columns</p>
-
-                <TextField.Input
-                  placeholder="8"
-                  value={columns === 0 ? "" : columns.toString()}
-                  size="1"
-                  min={2}
-                  max={16}
-                  className="!w-[70px]"
-                  onChange={(e) => {
-                    const value = parseInt(e.target.value)
-                    if (isNaN(value)) return update({ columns: 0 })
-
-                    update({ columns: value })
-                  }}
-                />
-              </div>
-
-              <div className="flex items-center gap-4 w-full">
-                <p className="text-1">Palette</p>
-
-                <RadixSelect
-                  value={palette}
-                  onChange={(p) => update({ palette: p as PaletteKey })}
-                  options={paletteOptions}
-                />
-              </div>
-
-              <div className="flex items-center gap-4 w-full">
-                <p className="text-1">Behavior</p>
-
-                <RadixSelect
-                  value={mode.toString()}
-                  onChange={(p) => update({ mode: p as PixelMode })}
-                  options={modeOptions}
-                />
-              </div>
-            </div>
+      <RightClickMenu id={id} show={isSettings} toggle={toggle}>
+        <div className="border-2 border-crimson-9 rounded-2 relative hover:border-cyan-9">
+          <div
+            className="absolute hidden group-hover:flex right-2 top-1 bg-gray-1 p-1 rounded-6 text-cyan-11 hover:bg-cyan-9 hover:text-gray-1"
+            onClick={toggle}
+          >
+            {isSettings ? <EyeClosedIcon /> : <MixerHorizontalIcon />}
           </div>
-        )}
-      </div>
+
+          <div className="">
+            {!isDrawable && (
+              <div className="px-4 py-3 font-mono text-1 text-crimson-10">
+                Columns must be more than 1!
+              </div>
+            )}
+
+            {isDrawable && (
+              <div
+                className=""
+                style={{ height: `${height}px`, width: `${width}px` }}
+              >
+                <canvas
+                  ref={(r) => (canvasRef.current = r)}
+                  className="h-[180px]"
+                  style={{ height, width }}
+                />
+              </div>
+            )}
+          </div>
+
+          {isSettings && (
+            <div className="flex flex-col font-mono bg-gray-1">
+              <div className="flex flex-col max-w-[160px] gap-y-3 px-3 py-3">
+                <div className="flex items-center gap-4">
+                  <p className="text-1">Columns</p>
+
+                  <TextField.Input
+                    placeholder="8"
+                    value={columns === 0 ? "" : columns.toString()}
+                    size="1"
+                    min={2}
+                    max={16}
+                    className="!w-[70px]"
+                    onChange={(e) => {
+                      const value = parseInt(e.target.value)
+                      if (isNaN(value)) return update({ columns: 0 })
+
+                      update({ columns: value })
+                    }}
+                  />
+                </div>
+
+                <div className="flex items-center gap-4 w-full">
+                  <p className="text-1">Palette</p>
+
+                  <RadixSelect
+                    value={palette}
+                    onChange={(p) => update({ palette: p as PaletteKey })}
+                    options={paletteOptions}
+                  />
+                </div>
+
+                <div className="flex items-center gap-4 w-full">
+                  <p className="text-1">Behavior</p>
+
+                  <RadixSelect
+                    value={mode.toString()}
+                    onChange={(p) => update({ mode: p as PixelMode })}
+                    options={modeOptions}
+                  />
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </RightClickMenu>
     </div>
   )
 }
