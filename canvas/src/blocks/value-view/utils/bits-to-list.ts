95941c926973cff87dfecc86385087ee9bb02d20
Phoomparin Mano <phoomparin@gmail.com>
2023-12-18 19:48:55 +0700

    feat: padded bits

--- canvas/src/blocks/value-view/utils/bits-to-list.ts
+++ canvas/src/blocks/value-view/utils/bits-to-list.ts
@@ -1,6 +1,15 @@
 export const bitsToList = (nums: number[]): boolean[][] =>
   nums.map((n) => {
-    // console.log(`n -> ${n.toString(2)}`)
+    const value = n.toString(2).split("").map(Number).map(Boolean)
 
-    return n.toString(2).padEnd(8, "0").split("").map(Number).map(Boolean)
+    if (value.length < 16) {
+      const pad = Math.ceil(value.length / 8) * 8
+      const needed = pad - value.length
+
+      if (needed > 0) {
+        for (let i = 0; i < needed; i++) value.push(false)
+      }
+    }
+
+    return value
   })
