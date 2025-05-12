16a3e4e89f2068a99b9f590c99546fd656526ab2
Phoomparin Mano <phoomparin@gmail.com>
2025-05-12 12:17:38 +0700

    fix tailwind

--- canvas/vite.config.ts
+++ canvas/vite.config.ts
@@ -6,11 +6,13 @@ import glsl from "vite-plugin-glsl"
 import { ViteRsw } from "vite-plugin-rsw"
 import awaits from "vite-plugin-top-level-await"
 import tsconfigPaths from "vite-tsconfig-paths"
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
     react(),
+    tailwindcss(),
     awaits(),
     ViteRsw(),
     glsl(),
