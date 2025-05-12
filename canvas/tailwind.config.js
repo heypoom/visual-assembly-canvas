ef91e558070d906f2bfd17f2c8f54ad1d37fc836
Phoomparin Mano <phoomparin@gmail.com>
2025-05-12 12:34:06 +0700

    restore radix colors preset

--- canvas/tailwind.config.js
+++ canvas/tailwind.config.js
@@ -1,10 +1,7 @@
-import {radixThemePreset} from 'radix-themes-tw'
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  presets: [radixThemePreset],
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
